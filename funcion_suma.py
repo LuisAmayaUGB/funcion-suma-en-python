981ea1914f8e58b2501498b93219f43b1ff8a02e
luis antonio amaya villalta <amayantonio@hotmail.com>
2021-08-12 07:53:56 -0600

    Ana agregó la función de la resta

--- funcion_suma.py
+++ funcion_suma.py
@@ -2,7 +2,10 @@ def Suma(parametro1, parametro2):
     Resultado = parametro1 + parametro2
     return Resultado
 
-
+def Resta(parametro1, parametro2):
+    Resultado = parametro1 - parametro2
+    return Resultado
 
 
 print(Suma(3,4))
+print(Resta(3,4))
